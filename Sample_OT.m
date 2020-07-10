09d35210823d8eb1e7c2fda3615dd696418d10ec
mhdsedighi <mhd.sedighi@gmail.com>
2020-07-10 23:09:46 +0430

    initial test of Cartesian ode

--- Sample_OT.m
+++ Sample_OT.m
@@ -1,39 +1,67 @@
-% MAIN - Pendulum
-%
-% Demonstrates simple swing-up for a single pendulum with a torque motor.
-%
-
-clc; clear;
+clc; clear; close all
 addpath OptimTraj
 
-% Physical parameters of the pendulum
-p.k = 1;  % Normalized gravity constant
-p.c = 0.1;  % Normalized damping constant
+%parameters
+p.mu = 3.986005*10^5;
+Re=6378.14;
+
+%%%%%%%%%%%%%%%%%%%
+
+a_0=10*Re;
+e_0=0;
+incl_0=0;
+omega_0=0;
+RA_0=0;
+theta_0=0;
+
+a_f=11*Re;
+e_f=0;
+incl_f=0;
+omega_f=0;
+RA_f=0;
+theta_f=deg2rad(90);
+
+
+[r0,v0]=oe2rv([a_0 e_0 incl_0 omega_0 RA_0 theta_0],p.mu);
+[rf,vf]=oe2rv([a_f e_f incl_f omega_f RA_f theta_f],p.mu);
+X_0=[r0;v0];
+X_f=[rf;vf];
+
+
+low_bound=[5*Re 5*Re 5*Re 0 0 0];
+upp_bound=[20*Re 20*Re 20*Re 1 1 pi];
+
+F_max=1e-5;
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%
 
 % User-defined dynamics and objective functions
 problem.func.dynamics = @(t,x,u)( dynamics(x,u,p) );
-problem.func.pathObj = @(t,x,u)( u.^2 );
+problem.func.pathObj = @(t,x,u)( dot(u,u) );
 
 % Problem bounds
 problem.bounds.initialTime.low = 0;
 problem.bounds.initialTime.upp = 0;
-problem.bounds.finalTime.low = 0.5;
-problem.bounds.finalTime.upp = 2.5;
+problem.bounds.finalTime.low = 1000;
+problem.bounds.finalTime.upp = 10000;
 
-problem.bounds.state.low = [-2*pi; -inf];
-problem.bounds.state.upp = [2*pi; inf];
-problem.bounds.initialState.low = [0;0];
+problem.bounds.state.low = low_bound';
+problem.bounds.state.upp = upp_bound';
+problem.bounds.initialState.low = X_0;
 problem.bounds.initialState.upp = problem.bounds.initialState.low;
-problem.bounds.finalState.low = [pi;0];
-problem.bounds.finalState.upp = problem.bounds.finalState.low;
+problem.bounds.finalState.low = X_f;
+problem.bounds.finalState.upp = X_f;
 
-problem.bounds.control.low = -5; %-inf;
-problem.bounds.control.upp = 5; %inf;
+problem.bounds.control.low = -F_max*[1;0;0] ;
+problem.bounds.control.upp = F_max*[-1;0;0];
 
 % Guess at the initial trajectory
 problem.guess.time = [0,(problem.bounds.finalTime.low+problem.bounds.finalTime.upp)/2];
 problem.guess.state = [problem.bounds.initialState.low problem.bounds.finalState.low];
-problem.guess.control = [0, 0];
+problem.guess.control = [0 0;0 0;0 0];
 
 % Select a solver:
 problem.options.method = 'trapezoid';
@@ -41,25 +69,34 @@ problem.options.defaultAccuracy = 'medium';
 
 % Solve the problem
 soln = optimTraj(problem);
-t = soln.grid.time;
-q = soln.grid.state(1,:);
-dq = soln.grid.state(2,:);
-u = soln.grid.control;
+T = soln.grid.time;
+U = soln.grid.control;
 
-% Plot the solution:
-figure(1); clf;
 
+N=length(T);
+R=soln.grid.state(1:3,:);
+
+
+
+figure
+hold on
+axis equal
+grid minor
+view(25,45)
+plot3(0,0,0,'ro')
+plot3(R(1,:),R(2,:),R(3,:),'b')
+plot3(R(1,:),R(2,:),R(3,:),'k.')
+
+figure
+title('Controls')
 subplot(3,1,1)
-plot(t,q)
-ylabel('q')
-title('Single Pendulum Swing-Up');
-
+plot(T,U(1,:),'k.')
 subplot(3,1,2)
-plot(t,dq)
-ylabel('dq')
-
+plot(T,U(2,:),'k.')
 subplot(3,1,3)
-plot(t,u)
-ylabel('u')
+plot(T,U(3,:),'k.')
+
+
+
 
 
--- dynamics.m
+++ dynamics.m
@@ -1,14 +1,35 @@
 function dx = dynamics(x,u,p)
-% dx = dynamics(x,u,p)
-%
-% Computes the dynamics for the simple pendulum
-%
 
-q = x(1,:);
-dq = x(2,:);
 
-k = p.k;    c = p.c;
-ddq = -c*dq - k*sin(q) + u;
-dx = [dq;ddq];
+mu=p.mu;
+
+
+
+X = x(1,:);
+Y = x(2,:);
+Z = x(3,:);
+Xdot = x(4,:);
+Ydot = x(5,:);
+Zdot = x(6,:);
+
+force_r=u(1,:);
+force_s=u(2,:);
+force_w=u(3,:);
+
+r=sqrt(X.^2+Y.^2+Z.^2);
+
+dir_r=[X;Y;Z]./r;
+
+c1=-mu./r.^3;
+
+dX = Xdot;
+dY = Ydot;
+dZ = Zdot;
+dXdot = c1.*X+dir_r(1,:).*force_r;
+dYdot = c1.*Y+dir_r(2,:).*force_r;
+dZdot = c1.*Z+dir_r(3,:).*force_r;
+
+
+dx=[dX;dY;dZ;dXdot;dYdot;dZdot];
 
 end
