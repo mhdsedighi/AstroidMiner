0287be7eec02b7816d1e88dfe44a8bbd3ad665f9
mhdsedighi <mhd.sedighi@gmail.com>
2020-08-17 14:34:40 +0430

    tested with more sophisticated moment of inertia

--- combo_dynamics.m
+++ combo_dynamics.m
@@ -6,12 +6,12 @@ N_sat=params.N_sat;
 Force_Vectors=params.Force_Vectors;
 Moment_Vectors=params.Moment_Vectors;
 
-Ixx=100;
-Iyy=100;
-Izz=100;
-Ixy=0;
-Ixz=0;
-Iyz=0;
+Ixx=1000;
+Iyy=1000;
+Izz=1000;
+Ixy=50;
+Ixz=40;
+Iyz=60;
 
 
 p=x(1,:);
