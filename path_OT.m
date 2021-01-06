cdaf2bc3841c17dcfbc0074b150a6d31af886796
mhdsedighi <mhd.sedighi@gmail.com>
2021-01-06 22:02:12 +0330

    a solve for path only

---
+++ path_OT.m
@@ -0,0 +1,226 @@
+clc; clear; close all
+addpath OptimTraj
+addpath chebfun
+load('params')
+
+%parameters
+params.mu = 3.986005*10^5;
+Re=6378.14;
+
+% params.Ixx=1000;
+% params.Iyy=1000;
+% params.Izz=1000;
+% params.Ixy=0;
+% params.Ixz=0;
+% params.Iyz=0;
+
+%%%%%%%%%%%%%%%%%%%
+
+a_0=100*Re;
+e_0=0;
+incl_0=deg2rad(70);
+omega_0=0;
+RA_0=0;
+theta_0=deg2rad(0);
+
+a_f=10*Re;
+e_f=0;
+incl_f=deg2rad(0);
+omega_f=0;
+RA_f=0;
+theta_f=deg2rad(10);
+
+min_revolution=1;
+max_revolution=2;
+
+min_days=0;
+max_days=10;
+
+oe_0=[a_0 e_0 incl_0 omega_0 RA_0 theta_0];
+oe_f=[a_f e_f incl_f omega_f RA_f theta_f];
+mee_0=oe2mee(oe_0,params.mu)';
+mee_f=oe2mee(oe_f,params.mu)';
+
+% mee_f=mee_0;
+
+% quat_0=eul2quat(deg2rad([20 30 40]));
+% quat_f=eul2quat(deg2rad([30 40 50]));
+
+state_0=mee_0;
+state_f=mee_f;
+
+% low_bound=[5*Re -inf -inf -inf -inf -inf];
+% upp_bound=[20*Re 1 1 1 1 pi];
+
+thrust_max=0.0000001;
+% thrust_max=1;
+% max_angular_speed_end=1e-5;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%
+
+% User-defined dynamics and objective functions
+problem.func.dynamics = @(t,x,u)( path_dynamics(x,u,params) );
+problem.func.pathObj = @(t,x,u)( pathcost(t,x,u) );
+
+% Problem bounds
+problem.bounds.initialTime.low = 0;
+problem.bounds.initialTime.upp = 0;
+problem.bounds.finalTime.low = min_days*24*3600;
+problem.bounds.finalTime.upp = max_days*24*3600;
+
+problem.bounds.state.low = [0.7*min([a_0 a_f])   -inf -inf -inf -inf -inf]';
+problem.bounds.state.upp = [1.3*max([a_0 a_f])   1    1     1    1  inf]';
+problem.bounds.initialState.low = state_0;
+problem.bounds.initialState.upp = state_0;
+% 
+% problem.bounds.initialState.low(end)=0;
+% problem.bounds.initialState.upp(end)=2*pi;
+
+
+% problem.bounds.finalState.low = state_f;
+% problem.bounds.finalState.upp = state_f;
+
+
+% Guess at the initial trajectory
+problem.guess.time = [0,0.5*(min_days+max_days)*24*3600];
+problem.guess.time = [0,max_days*24*3600];
+state_f(6)=mee_f(end)+max_revolution*2*pi;
+problem.guess.state = [state_0  state_f];
+% problem.guess.control = zeros(3,2)+0.1*thrust_max;
+problem.guess.control = zeros(3,2);
+
+
+problem.bounds.control.low = 0*ones(3,1);
+problem.bounds.control.upp = thrust_max*ones(3,1);
+
+
+
+
+% state_f(1:3)=-max_angular_speed_end;
+% state_f(4:7)=-1;
+state_f(6)=mee_0(end)+min_revolution*2*pi;
+problem.bounds.finalState.low = state_f;
+% state_f(1:3)=max_angular_speed_end;
+% state_f(4:7)=1;
+state_f(6)=mee_f(end)+max_revolution*2*pi;
+state_f(6)=1000;
+problem.bounds.finalState.upp = state_f;
+
+
+
+% Select a solver:
+% problem.options.method = 'rungeKutta';
+% problem.options.method = 'chebyshev';
+
+
+% problem.options.method = 'trapezoid';
+% problem.options.method = 'multiCheb';
+
+problem.options.defaultAccuracy = 'medium';
+
+% problem.options.rungeKutta.nSegment=100;
+% problem.options.trapezoid.nGrid=200;
+
+% problem.options.nlpOpt.MaxFunEvals=1e6;
+% problem.options.nlpOpt.MaxIter=1e5;
+
+% problem.options.chebyshev.nColPts=30;
+
+% problem.options.multiCheb.nColPts=30;
+
+% method = 'trapezoid'; %  <-- this is robust, but less accurate
+% method = 'direct'; %  <-- this is robust, but some numerical artifacts
+% method = 'rungeKutta';  % <-- slow, gets a reasonable, but sub-optimal soln
+method = 'orthogonal';    %  <-- this usually finds bad local minimum
+% method = 'gpops';      %  <-- fast, but numerical problem is maxTorque is large
+
+switch method
+    case 'direct'
+        problem.options(1).method = 'trapezoid';
+        problem.options(1).trapezoid.nGrid = 20;
+        
+        problem.options(2).method = 'trapezoid';
+        problem.options(2).trapezoid.nGrid = 40;
+        
+        problem.options(3).method = 'hermiteSimpson';
+        problem.options(3).hermiteSimpson.nSegment = 20;
+        
+    case 'trapezoid'
+        problem.options(1).method = 'trapezoid';
+        problem.options(1).trapezoid.nGrid = 30;
+        problem.options(2).method = 'trapezoid';
+        problem.options(2).trapezoid.nGrid = 40;
+        problem.options(3).method = 'trapezoid';
+        problem.options(3).trapezoid.nGrid = 60;
+        
+    case 'rungeKutta'
+        problem.options(1).method = 'rungeKutta';
+        problem.options(1).defaultAccuracy = 'low';
+        
+        problem.options(2).method = 'rungeKutta';
+        problem.options(2).defaultAccuracy = 'medium';
+       
+        
+        
+        
+        
+        
+    case 'orthogonal'
+        step=0;
+        
+        
+        step=step+1;
+        problem.options(step).method = 'chebyshev';
+        problem.options(step).chebyshev.nColPts =100;
+        problem.options(step).defaultAccuracy = 'low';
+        problem.options(step).nlpOpt.MaxFunEvals=1e5;
+%         problem.options.nlpOpt.MaxIter=500;
+        
+%         step=step+1;
+%         problem.options(step).method = 'chebyshev';
+%         problem.options(step).chebyshev.nColPts =20;
+%         problem.options(step).defaultAccuracy = 'low';
+% %         problem.options(step).nlpOpt.MaxFunEvals=1e6;
+%         
+%         
+%                 step=step+1;
+%         problem.options(step).method = 'chebyshev';
+%         problem.options(step).chebyshev.nColPts =60;
+%         problem.options(step).defaultAccuracy = 'medium';
+%         problem.options(step).nlpOpt.MaxFunEvals=1e5;
+        
+%                 step=step+1;
+%                 problem.options(step).method = 'trapezoid';
+%                 problem.options(step).chebyshev.nColPts = 40;
+%                 problem.options(step).defaultAccuracy = 'medium';
+% %                 problem.options(step).nlpOpt.MaxFunEvals=1e5;
+        
+        
+%                 problem.options(2).method = 'chebyshev';
+%                 problem.options(2).chebyshev.nColPts =100;
+%                 problem.options(2).defaultAccuracy = 'medium';
+%                 problem.options(2).nlpOpt.MaxFunEvals=1e5;
+       
+%          step=step+1;
+%         problem.options(step).method = 'rungeKutta';
+%         problem.options(step).chebyshev.nColPts = 50;
+%         problem.options(step).defaultAccuracy = 'medium';
+%         problem.options(step).nlpOpt.MaxFunEvals=1e5;
+    case 'gpops'
+        problem.options(1).method = 'gpops';
+        
+end
+
+
+% Solve the problem
+soln = optimTraj(problem);
+T = soln(end).grid.time;
+U = soln(end).grid.control;
+
+
+
+T(end)/3600/24
+
+plotting_path
---
+++ plotting_path.m
@@ -0,0 +1,176 @@
+N=length(T);
+R=zeros(3,N);
+V=zeros(3,N);
+OE=zeros(6,N);
+% EUL=zeros(3,N);
+% 
+for i=1:N
+    [r,v]=mee2rv(soln(end).grid.state(1:6,i)',params.mu);
+%     quat=soln(end).grid.state(4:7,i);
+    
+    oe=rv2oe(r,v,params.mu);
+    R(:,i)=r;
+    V(:,i)=v;
+    OE(:,i)=oe';
+%     EUL(:,i)=quat2eul(quat')';
+end
+
+Force_history_xyz=zeros(3,N);
+% Force_mag=zeros(1,N);
+for i=1:N
+    Force_history_xyz(:,i)=rsw2xyz(U(:,i),R(:,i),V(:,i));
+%     Force_mag(i)=norm(Force_history_xyz(:,i));
+end
+
+% EUL=rad2deg(EUL);
+
+close all
+
+figure
+hold on
+axis equal
+grid minor
+view(25,45)
+plot3(0,0,0,'ro')
+plot_earth
+plot3(R(1,:),R(2,:),R(3,:),'b')
+plot3(R(1,:),R(2,:),R(3,:),'r.')
+
+
+% quiver3(R(1,:),R(2,:),R(3,:),Force_history_xyz(1,:),Force_history_xyz(2,:),Force_history_xyz(3,:),1.3,'k')
+
+
+
+figure
+hold on
+plot(T,U(:,:))
+% plot(T,U(:,:),'k.')
+ylabel('Thrusts')
+xlabel('t (s)')
+legend
+
+
+
+
+% figure
+% subplot(3,2,1)
+% hold on
+% plot(T(1),state_0(1),'bo')
+% plot(T(end),state_f(1),'bo')
+% plot(T,soln(end).grid.state(1,:))
+% plot(T,soln(end).grid.state(1,:),'r.')
+% ylabel('\omega_x')
+% subplot(3,2,3)
+% hold on
+% plot(T(1),state_0(2),'bo')
+% plot(T(end),state_f(2),'bo')
+% plot(T,soln(end).grid.state(2,:))
+% plot(T,soln(end).grid.state(2,:),'r.')
+% ylabel('\omega_y')
+% subplot(3,2,5)
+% hold on
+% plot(T(1),state_0(3),'bo')
+% plot(T(end),state_f(3),'bo')
+% plot(T,soln(end).grid.state(3,:))
+% plot(T,soln(end).grid.state(3,:),'r.')
+% ylabel('\omega_z')
+% xlabel('t (s)')
+
+% abs_omega=sqrt(soln(end).grid.state(1,:).^2+soln(end).grid.state(2,:).^2+soln(end).grid.state(3,:).^2);
+% 
+% subplot(3,2,2)
+% hold on
+% plot(T,abs_omega)
+% plot(T,abs_omega,'k.')
+% ylabel('\omega')
+% xlabel('t (s)')
+
+
+
+% subplot(3,2,2)
+% plot(T,EUL(1,:),'b.')
+% ylabel('\phi')
+% subplot(3,2,4)
+% plot(T,EUL(2,:),'b.')
+% ylabel('\theta')
+% subplot(3,2,6)
+% plot(T,EUL(3,:),'b.')
+% ylabel('\psi')
+
+
+% 
+figure
+subplot(3,2,1)
+hold on
+plot(T(1),mee_0(1),'bo')
+plot(T(end),mee_f(1),'bo')
+plot(T,soln(end).grid.state(1,:),'r.')
+ylabel('mee1')
+subplot(3,2,2)
+hold on
+plot(T(1),mee_0(2),'bo')
+plot(T(end),mee_f(2),'bo')
+plot(T,soln(end).grid.state(2,:),'r.')
+ylabel('mee2')
+subplot(3,2,3)
+hold on
+plot(T(1),mee_0(3),'bo')
+plot(T(end),mee_f(3),'bo')
+plot(T,soln(end).grid.state(3,:),'r.')
+ylabel('mee3')
+subplot(3,2,4)
+hold on
+plot(T(1),mee_0(4),'bo')
+plot(T(end),mee_f(4),'bo')
+plot(T,soln(end).grid.state(4,:),'r.')
+ylabel('mee4')
+subplot(3,2,5)
+hold on
+plot(T(1),mee_0(5),'bo')
+plot(T(end),mee_f(5),'bo')
+plot(T,soln(end).grid.state(5,:),'r.')
+ylabel('mee5')
+subplot(3,2,6)
+hold on
+plot(T(1),mee_0(6),'bo')
+plot(T,soln(end).grid.state(6,:),'r.')
+ylabel('mee6')
+
+
+
+figure
+subplot(3,2,1)
+hold on
+plot(T(1),oe_0(1),'bo')
+plot(T(end),oe_f(1),'bo')
+plot(T,OE(1,:),'r.')
+ylabel('a')
+subplot(3,2,2)
+hold on
+plot(T(1),oe_0(2),'bo')
+plot(T(end),oe_f(2),'bo')
+plot(T,OE(2,:),'r.')
+ylabel('e')
+subplot(3,2,3)
+hold on
+plot(T(1),oe_0(3),'bo')
+plot(T(end),oe_f(3),'bo')
+plot(T,rad2deg(OE(3,:)),'r.')
+ylabel('i')
+subplot(3,2,4)
+hold on
+plot(T(1),oe_0(4),'bo')
+plot(T(end),oe_f(4),'bo')
+plot(T,rad2deg(OE(4,:)),'r.')
+ylabel('\omega')
+subplot(3,2,5)
+hold on
+plot(T(1),oe_0(5),'bo')
+plot(T(end),oe_f(5),'bo')
+plot(T,rad2deg(OE(5,:)),'r.')
+ylabel('\Omega')
+subplot(3,2,6)
+hold on
+plot(T(1),oe_0(6),'bo')
+plot(T,rad2deg(OE(6,:)),'r.')
+ylabel('\theta')
