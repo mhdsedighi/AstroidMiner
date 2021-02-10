fba2a8723067ce894d33285153d5492127f6a4df
mhdsedighi <mhd.sedighi@gmail.com>
2021-02-10 16:42:56 +0330

    rand_gen can now produce initial guess for optimization

--- rand_gen.m
+++ rand_gen.m
@@ -1,5 +1,12 @@
 function mat=rand_gen(m,n,mini,maxi)
 
-mat=mini+(maxi-mini)*rand(m,n);
+if length(mini)==1
+    mat=mini+(maxi-mini)*rand(m,n);
+else
+    mat=zeros(1,n);
+    for i=1:n  
+        mat(1,i)=mini(i)+(maxi(i)-mini(i))*rand;  
+    end
+end
 
 end
