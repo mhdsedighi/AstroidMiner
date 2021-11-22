0beaf70cecad66a54d8053722b0c5483ff431da2
mhdsedighi <mhd.sedighi@gmail.com>
2021-11-22 10:39:08 +0330

    nice exact-force run. (thrust is variable)

--- initial_sim2.m
+++ initial_sim2.m
@@ -50,28 +50,28 @@ params.assume_ellipsoid=assume_ellipsoid;
 %%%%%%%%%%%%%%%%%%%
 AU=1.496e+8;
 
-% a_0=1.1896*AU;
-% e_0=0.1902;
-% incl_0=deg2rad(5.8837);
-% omega_0=deg2rad(211.43);
-% RA_0=deg2rad(251.62);
-% theta_0=deg2rad(0);
-
-
 a_0=1.1896*AU;
-e_0=0.1;
-incl_0=deg2rad(5);
-omega_0=deg2rad(5);
-RA_0=deg2rad(20);
-theta_0=deg2rad(10);
+e_0=0.1902;
+incl_0=deg2rad(5.8837);
+omega_0=deg2rad(211.43);
+RA_0=deg2rad(251.62);
+theta_0=deg2rad(0);
 
 
-a_f=a_0*1.1;
-e_f=0.1;
-incl_f=deg2rad(4);
-omega_f=deg2rad(0);
-RA_f=deg2rad(25);
-theta_f=theta_0;
+% a_0=1.1896*AU;
+% e_0=0.1;
+% incl_0=deg2rad(5);
+% omega_0=deg2rad(5);
+% RA_0=deg2rad(20);
+% theta_0=deg2rad(10);
+
+
+% a_f=a_0*1.1;
+% e_f=0.15;
+% incl_f=deg2rad(4);
+% omega_f=deg2rad(3);
+% RA_f=deg2rad(21);
+% theta_f=theta_0;
 
 % a_0=1.1896*AU/100;
 % e_0=0.1;
@@ -88,12 +88,12 @@ theta_f=theta_0;
 % RA_0=deg2rad(0);
 % theta_0=deg2rad(0);
 
-% a_f=149598023;
-% e_f=0.0167086;
-% incl_f=deg2rad(7.155);
-% omega_f=deg2rad(114.20783);
-% RA_f=deg2rad(-11.26064);
-% theta_f=deg2rad(0);
+a_f=149598023;
+e_f=0.0167086;
+incl_f=deg2rad(7.155);
+omega_f=deg2rad(114.20783);
+RA_f=deg2rad(-11.26064);
+theta_f=deg2rad(0);
 % 
 % e_f=e_0;
 % omega_f=omega_0;
